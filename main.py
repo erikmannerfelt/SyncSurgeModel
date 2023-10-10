1f01577d3316c56d3256d081bbe866485903b0bd
Claudio <spaziotemporale.com@gmail.com>
2023-10-10 11:54:04 +0200

    Created function to generate surges

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+import random as rng
 
 
 def generate_test_cases(
-    n_glaciers: int,
-    n_iters: int,
-    min_period: float,
-    max_period: float,
-    random_state: int | None = None,
+        n_glaciers: int,
+        n_iters: int,
+        min_period: float,
+        max_period: float,
+        random_state: int | None = None,
 ) -> tuple[np.ndarray, np.ndarray]:
     """
     Generate (pseudo-)random test cases of surge periodicities and phases.
@@ -67,6 +68,28 @@ def generate_test_cases(
     return phases, periods
 
 
+# generates the surge events for every glacier according to a normal distribution with mean period and variance variances, we
+# \then offset by the value contained in the vector phases
+def generate_glaciers(
+        phases: np.ndarray,
+        periods: np.ndarray,
+        variances: np.array,
+        n_surges: int = 200,
+        random_state: int | None = None
+) -> np.array:
+    rng: np.random.Generator = np.random.default_rng(random_state)
+    n_glaciers = len(periods)
+    # generate standard normal distributions
+    surges = rng.normal(0, 1, size=(n_glaciers, n_surges))
+    # trasform standard normal distribution in normal distribution with mean periods and varianve var
+    surges = surges + np.reshape(periods, (-1, 1))
+    surges = surges * np.reshape(np.sqrt(variances), (-1, 1))
+
+    np.cumsum(surges, axis=1, out=surges)
+    surges = surges + np.reshape(phases, (-1, 1))
+    return surges
+
+
 def count_surges(phases: np.ndarray, periods: np.ndarray, test_year: float, sync_threshold: float) -> np.ndarray:
     """
     Count the amount of surges that should occur at a given year within a given acceptable range.
@@ -92,15 +115,15 @@ def count_surges(phases: np.ndarray, periods: np.ndarray, test_year: float, sync
     n_surges = np.count_nonzero(is_surging, axis=0)
 
     return n_surges
-    
+
 
 def main(
-    n_glaciers: int = 15,
-    n_iters: int = int(1e6),
-    min_period: float = 75.0,
-    max_period: float = 150.0,
-    random_state: int = 1,
-    test_year: int = 0,
+        n_glaciers: int = 15,
+        n_iters: int = int(1e6),
+        min_period: float = 75.0,
+        max_period: float = 150.0,
+        random_state: int = 1,
+        test_year: int = 0,
 ):
     """
     Run the main simulation.
@@ -129,14 +152,16 @@ def main(
     all_in_phase[:, :] = phases[[0], :]
 
     plt.figure(figsize=(8, 5))
-    for i, (scenario, phase_to_use) in enumerate([("Random phase", phases), ("Synchronized phase", all_in_phase)], start=1):
+    for i, (scenario, phase_to_use) in enumerate([("Random phase", phases), ("Synchronized phase", all_in_phase)],
+                                                 start=1):
 
         plt.subplot(1, 2, i)
         # Test the probabilities of between 2 and N glaciers
         n_glaciers_arr = np.arange(2, n_glaciers + 1)
         # Test lots of different thresholds to accept as synchronous and plot them all
         for sync_threshold in [5., 10., 15., 30., 50., 75., 150.]:
-            n_surges = count_surges(phases=phase_to_use, periods=periods, test_year=test_year, sync_threshold=sync_threshold)
+            n_surges = count_surges(phases=phase_to_use, periods=periods, test_year=test_year,
+                                    sync_threshold=sync_threshold)
 
             n_surge_likelihood = 100 * np.count_nonzero(n_surges[:, None] >= n_glaciers_arr[None, :], axis=0) / n_iters
 
@@ -155,6 +180,7 @@ def main(
     plt.savefig(out_path, dpi=600)
 
     plt.show()
+
 
 if __name__ == "__main__":
     main()
