269c18cd69bebf5860a757b99ffde9d4bac8afb4
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2025-08-15 09:46:12 +0200

    Added synchronized phase surge figure and updated figures.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
@@ -137,51 +138,139 @@ def count_surges(phases: np.ndarray, periods: np.ndarray, sync_threshold: float,
     return n_surges
 
 
-def calculate_surge_frequency_spread(baseline_frequency: float = 1.5, year_span: float = 10., n_tests: int = 15, n_simulations: int = 5000):
+def calculate_random_phase_concurrence_likelihood(baseline_frequencies: float | list[float] = 3., year_span: float = 10., n_tests: int = 15, n_simulations: int = 500):
     import tqdm
     import tqdm.contrib.concurrent
+
+    if not isinstance(baseline_frequencies, Iterable):
+        baseline_frequencies = [baseline_frequencies]
 
     test_mean_periods = np.linspace(50, 300, n_tests)
     test_std_periods = np.linspace(10, 200, n_tests)
     test_n_glaciers = np.linspace(50, 1000, n_tests, dtype=int)
     combinations = list(itertools.product(test_mean_periods, test_std_periods, test_n_glaciers))
 
-    bins = np.linspace(0, 3, 300)
+    frequency_bins = np.linspace(0, 5, 300)
     histograms = []
+
+    plt.figure(figsize=(6, 4))
+    for baseline_frequency in baseline_frequencies:
+        def process(combination):
+            mean_period, std_period, n_glaciers = combination        
+            phases, periods = generate_test_cases_std(n_glaciers=n_glaciers, n_iters=n_simulations, mean_period=mean_period, std_period=std_period,random_state=int(f"{mean_period:.0f}{std_period:.0f}{n_glaciers}"))
+            # Clip the periodicity to min=10 years because we've never seen anything faster than that
+            periods = np.clip(periods, a_min=10, a_max=None)
+   
+            n_surges = count_surges(phases=phases, periods=periods, sync_threshold=year_span / 2) / year_span
+
+            if abs(n_surges.mean() - baseline_frequency) > 0.25:
+                return
+
+            hist= np.histogram(n_surges, bins=frequency_bins)[0] 
+            return hist
+
+        result = tqdm.contrib.concurrent.thread_map(process, combinations)
+        histograms = [l for l in result if l is not None]
+        histogram = np.sum(histograms, axis=0)
+
+        histogram = 100 * histogram / np.sum(histogram)
+        bin_centers = (frequency_bins[:-1] + frequency_bins[1:]) / 2
+        cumul_hist = np.cumsum(histogram)
+
+        plt.plot(bin_centers[1:][np.diff(cumul_hist) > 0], cumul_hist[1:][np.diff(cumul_hist) > 0], label=r"$\overline{F}$ = " + f"{baseline_frequency} / yr")
+    plt.grid(alpha=0.3)
+
+    plt.title("Probabilities of random phase surge concurrence")
+    plt.xlabel(f"Momentary frequency (F; surges / yr)")
+    plt.ylabel("Cumulative likelihood (%)")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("figures/random_surge_likelihoods.jpg", dpi=600)
+    plt.show()
+    return
+
+    n_over_threshold = np.sum(histogram[frequency_bins[1:] >= 3.])
+    frac_over_threshold = n_over_threshold / np.sum(histogram)
+    print(f"3: {n_over_threshold=}, {frac_over_threshold=}, n_total={np.sum(histogram)}")
+    n_over_threshold = np.sum(histogram[frequency_bins[1:] >= 2.])
+    frac_over_threshold = n_over_threshold / np.sum(histogram)
+    print(f"2: {n_over_threshold=}, {frac_over_threshold=}, n_total={np.sum(histogram)}")
+    
+    plt.bar(frequency_bins[:-1] + np.diff(frequency_bins) /2, np.sum(histograms, axis=0))
+    plt.ylabel("Simulation frequency")
+    plt.xlabel("N surges / 10 years")
+
+    plt.show()
+
+
+def calculate_synced_phase_concurrence_likelihood(baseline_frequency: float = 1.5, year_span: float = 10., n_tests: int = 15, n_simulations: int = 5000):
+    
+    import tqdm
+    import tqdm.contrib.concurrent
+
+    # Generate all combinations of the test cases below
+    test_mean_periods = np.linspace(50, 300, n_tests)
+    test_std_periods = np.linspace(10, 200, n_tests)
+    test_n_glaciers = np.linspace(50, 1000, n_tests, dtype=int)
+    combinations = list(itertools.product(test_mean_periods, test_std_periods, test_n_glaciers))
+
+    # The frequencies to bin can vary between 0 and 100 (the 100-10000 is for extreme events)
+    frequency_bins = np.r_[np.linspace(0, 100, 300), 10000]
+    # The years normalized by mean periodicity (0-10 mean periods)
+    years_norm = np.linspace(0, 10, 300)
 
     def process(combination):
         mean_period, std_period, n_glaciers = combination        
         phases, periods = generate_test_cases_std(n_glaciers=n_glaciers, n_iters=n_simulations, mean_period=mean_period, std_period=std_period,random_state=int(f"{mean_period:.0f}{std_period:.0f}{n_glaciers}"))
         # Clip the periodicity to min=10 years because we've never seen anything faster than that
         periods = np.clip(periods, a_min=10, a_max=None)
-   
+
         n_surges = count_surges(phases=phases, periods=periods, sync_threshold=year_span / 2) / year_span
 
         if abs(n_surges.mean() - baseline_frequency) > 0.25:
             return
 
-        hist= np.histogram(n_surges, bins=bins, range=(0, 3))[0] 
-        return hist
+        # After filtering to only keep representative baseline frequencies (if clause above), set all phases to 0
+        # This means that all surges will begin on year=0
+        phases[:, :] = 0.
+
+        # For all (normalized) years, count the amount of surges that occurred.
+        n_surges_arr = np.zeros((years_norm.shape[0], frequency_bins.shape[0] - 1), dtype=int)
+        for i, year_norm in enumerate(years_norm):
+            n_surges = count_surges(phases=phases, periods=periods, sync_threshold=year_span / 2, test_year=year_norm * mean_period) / year_span
+            n_surges_arr[i, :] = np.histogram(n_surges, bins=frequency_bins)[0]
+
+        return n_surges_arr
 
     result = tqdm.contrib.concurrent.thread_map(process, combinations)
     histograms = [l for l in result if l is not None]
     histogram = np.sum(histograms, axis=0)
+    bin_centers = (frequency_bins[:-1] + frequency_bins[1:]) / 2
 
-    n_over_threshold = np.sum(histogram[bins[1:] >= 3.])
-    frac_over_threshold = n_over_threshold / np.sum(histogram)
-    print(f"3: {n_over_threshold=}, {frac_over_threshold=}, n_total={np.sum(histogram)}")
-    n_over_threshold = np.sum(histogram[bins[1:] >= 2.])
-    frac_over_threshold = n_over_threshold / np.sum(histogram)
-    print(f"2: {n_over_threshold=}, {frac_over_threshold=}, n_total={np.sum(histogram)}")
-    
-    plt.bar(bins[:-1] + np.diff(bins) /2, np.sum(histograms, axis=0))
-    plt.ylabel("Simulation frequency")
-    plt.xlabel("N surges / 10 years")
+    plt.figure(figsize=(6, 4))
+    # Count and plot the likelihood of a surge frequency above these numbers
+    for num in [2., 3.]:
+        frequent_surge_likelihood = 100 * histogram[:, frequency_bins[:-1] >= num].sum(axis=1) / histogram.sum(axis=1)
+
+        plt.plot(years_norm, frequent_surge_likelihood, label=f"≥{num} / yr")
+
+    # Count and plot the likelihood of a surge frequency below these numbers
+    for num in [1.5]:
+        frequent_surge_likelihood = 100 * histogram[:, frequency_bins[:-1] <= num].sum(axis=1) / histogram.sum(axis=1)
+
+        plt.plot(years_norm, frequent_surge_likelihood, label=f"≤{num} / yr")
+
+    plt.legend()
+    plt.ylim(0, 100)
+    plt.title(r"Likelihoods of synchonization for $\overline{F}$= "+f"{baseline_frequency} surges / year")
+    plt.xlabel(r"Time since synchronization (normalized periodicity; T / $\overline{T}$)")
+    plt.ylabel("Synchronization likelihood (%)")
+
+    plt.tight_layout()
+    plt.savefig(f"figures/synchronized_surge_likelihood_{str(baseline_frequency).replace('.', '-')}peryear.jpg", dpi=600)
+    # plt.yscale("log")
     plt.show()
-    
 
-    
-    
 
 def main(
     n_glaciers: int = 15,
